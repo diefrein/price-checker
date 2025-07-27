46c9daba7296275c23c60602d056185eb9dbcad3
diefrein <59264106+diefrein@users.noreply.github.com>
2025-07-27 23:26:29 +0300

    Send updates only when product price has changed (#27)
    Co-authored-by: Kirill Ustimenko <diefrein@gmail.com>

--- telegram-bot/src/main/java/ru/diefrein/pricechecker/bot/transport/kafka/consumer/PriceChangeProcessor.java
+++ telegram-bot/src/main/java/ru/diefrein/pricechecker/bot/transport/kafka/consumer/PriceChangeProcessor.java
@@ -1,11 +1,15 @@
 package ru.diefrein.pricechecker.bot.transport.kafka.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.diefrein.pricechecker.bot.bot.PriceCheckerBot;
 import ru.diefrein.pricechecker.bot.service.UserService;
 import ru.diefrein.pricechecker.bot.storage.entity.User;
 import ru.diefrein.pricechecker.bot.transport.kafka.dto.PriceChangeEvent;
 
 public class PriceChangeProcessor {
+
+    private static final Logger log = LoggerFactory.getLogger(PriceChangeProcessor.class);
 
     private final PriceCheckerBot bot;
     private final UserService userService;
@@ -17,12 +21,15 @@ public class PriceChangeProcessor {
     }
 
     void process(PriceChangeEvent event) {
-        User user = userService.findByCheckerUserId(event.userId());
-        bot.sendMessage(user.telegramId(), buildResponse(event));
+        log.info("Received PriceChangeEvent={}", event);
+        if (!event.newPrice().equals(event.oldPrice())) {
+            User user = userService.findByCheckerUserId(event.userId());
+            bot.sendMessage(user.telegramId(), buildResponse(event));
+        }
     }
 
     private String buildResponse(PriceChangeEvent event) {
-        return String.format("Received an update on %s: now price is %s, was: %s\nlink: %s",
+        return String.format("Received an update on %s:\n now price is %s, was: %s\nlink: %s",
                 event.productName(), event.newPrice(), event.oldPrice(), event.link());
     }
 }
