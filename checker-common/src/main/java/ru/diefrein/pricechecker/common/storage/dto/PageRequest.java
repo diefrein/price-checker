97ceef8c9afb9aeb0f694bfd314092a68387a80a
Kirill Ustimenko <diefrein@gmail.com>
2025-07-30 22:39:59 +0300

    [COMMON] Add value validation in PageRequest constructor

--- checker-common/src/main/java/ru/diefrein/pricechecker/common/storage/dto/PageRequest.java
+++ checker-common/src/main/java/ru/diefrein/pricechecker/common/storage/dto/PageRequest.java
@@ -1,4 +1,13 @@
 package ru.diefrein.pricechecker.common.storage.dto;
 
 public record PageRequest(long pageSize, long pageNumber) {
+
+    public PageRequest {
+        if (pageSize < 0) {
+            throw new IllegalArgumentException("PageSize cannot be less then zero");
+        }
+        if (pageNumber <= 0) {
+            throw new IllegalArgumentException("PageNumber cannot be less then or equal to zero");
+        }
+    }
 }
